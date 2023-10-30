1e9157a687cfb81cb471186c3acc7c0bc1334172
Natan Maia <natanmaiaplays@outlook.com>
2023-10-30 13:24:53 -0300

    uri support parte 1

---
+++ front/WorkoutHero/src/screens/PassChangeScreen/index.js
@@ -0,0 +1,97 @@
+import React, { useContext, useEffect, useState } from "react"
+import {View, Text, TextInput, Button, Image, TouchableOpacity} from "react-native"
+import { useNavigation } from "@react-navigation/native";
+import styles from "./style"
+import MyTextRegular from "../../components/MyText/MyTextRegular";
+import MyButtonRegular from "../../components/MyButton/MyButtonRegular";
+import MyTextInput from "../../components/MyTextInput/MyTextInput";
+import MyButtonThin from "../../components/MyButton/MyButtonThin.js"
+
+import { AuthContext } from "../../AuthContext"
+
+export default function LoginScreen() {
+
+    const navigation = useNavigation();
+
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ errorMsg, setErrorMsg ] = useState('')
+
+    const { tryLogin } = useContext(AuthContext);
+
+
+    const handlePassRestoreRequest = () => {
+        navigation.push('PASSRESTOREREQUEST', {});
+    }
+
+    const handleLogin = async () => {
+        // testar credenciais
+        try {
+            var response = await tryLogin(username, password)
+            //console.log(response)
+            navigation.reset({
+                index: 0, routes: [{name:'MAIN'}]
+            })
+        } catch (error) {
+            console.log(error)
+            setErrorMsg(error.message)
+        }
+
+        // navigation.reset({
+        //     index: 0, routes: [{name:'MAIN'}]
+        // })
+    }
+
+    return (
+        <>
+        <View style={styles.body}>
+            <View style={{height: 100}}/>
+            
+            <View style={styles.viewLogo}>
+                <Image style={styles.imgLogo}
+                source={require('../WelcomeScreen/WorkoutHero_Logo1.png')} />
+            </View>
+            
+            <View style={{height: 32}}/>
+
+            <View style={styles.viewContent}>
+                <MyTextRegular>  EMAIL</MyTextRegular>
+                <MyTextInput 
+                style={styles.textInput} autoComplete='email'
+                value={username} onChangeText={setUsername}
+                ></MyTextInput>
+                <View style={{height: 16}}/>
+                <MyTextRegular>  SENHA</MyTextRegular>
+                <MyTextInput style={styles.textInput} 
+                autoComplete='current-password'
+                value={password} onChangeText={setPassword}
+                secureTextEntry = {true}
+                ></MyTextInput>
+                <View style={{height: 16}}/>
+                <View style={styles.viewError}>
+                    <Text style={styles.textError}>{errorMsg}</Text>
+                </View>
+                <View style={{height: 16}}/>
+                <View  style={styles.viewButton}>
+                    <MyButtonRegular style={styles.button}
+                    title="Continuar"
+                    onPress={handleLogin}
+                    />
+                </View>
+                <View style={{height: 16}}/>
+                <View  style={styles.viewButton}>
+                    <TouchableOpacity style={styles.extraButton}
+                        onPress={handlePassRestoreRequest}
+                    >
+                        <MyTextRegular>Esqueci minha senha</MyTextRegular>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            
+        
+        </View>
+
+        </>
+    );
+}
---
+++ front/WorkoutHero/src/screens/PassChangeScreen/style.js
@@ -0,0 +1,57 @@
+import {StyleSheet} from 'react-native'
+import AppStyles from '../../AppStyles';
+
+const styles = StyleSheet.create({
+    body: {
+        backgroundColor: AppStyles.colors.background,
+        width: "100%", height:"100%",
+        padding: 24
+    },
+    viewLogo: {
+        //borderWidth: 4, borderColor: "red",
+        //backgroundColor:"#f00",
+        // flex: 0.6,
+        alignItems: 'center'
+        //width: "100%"
+    },
+    imgLogo: {  
+        width: 256, height:256,
+        resizeMode: 'contain'
+    },
+    viewContent: {
+        paddingHorizontal: 16,
+    },
+    textInput: {
+        //borderWidth: 4, borderColor: "red",
+        // borderWidth: 3,
+        // borderRadius: 16,
+        // paddingLeft: 8, paddingRight: 8
+    },
+    viewError: {
+        //backgroundColor: '#aaa',
+        //borderWidth: 4, borderColor: "red",
+        flex: 0.2,
+        height: 100,
+    },
+    textError: {
+        //backgroundColor: '#001',
+        color: '#c11',
+        textAlign: 'center'
+    },
+    viewButton: {
+        //borderWidth: 4, borderColor: "red",
+        alignItems: 'center',
+        // width: "100%"
+    },
+    button: {
+        width: "100%", backgroundColor: AppStyles.colors.accent
+    },
+    extraButton: {
+        backgroundColor: "#0000", borderWidth: 0,
+        width: "100%", 
+        color: "#000",
+        alignItems: 'center',
+    }
+});
+
+export default styles
